0b5412d1550211c6a70039bebf4e3783ffd43cc5
PhyoMinKhantXem <phyominkhant.xem@gmail.com>
2023-08-24 22:08:57 +0630

    Lee AMM
    Tyy char lote lee pl ya ml

--- js/script.js
+++ js/script.js
@@ -5,20 +5,32 @@ intercept.setAttribute("data-observer-intercept", "");
 header.before(intercept);
 
 const observer = new IntersectionObserver(([entry]) => {
-  header.classList.toggle("active", !entry.isIntersecting);
+    header.classList.toggle("active", !entry.isIntersecting);
 });
 
 observer.observe(intercept);
 
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+let sections = document.querySelectorAll("section");
 
 menu.onclick = () => {
-	menu.classList.toggle('fa-x');
-	navlist.classList.toggle('active');
+    menu.classList.toggle("fa-x");
+    navlist.classList.toggle("active");
 };
 
 window.onscroll = () => {
-	menu.classList.remove('fa-x');
-	navlist.classList.remove('active');
+    menu.classList.remove("fa-x");
+    navlist.classList.remove("active");
+    sections.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add("show-animate");
+        } else {
+            sec.classList.remove("show-animate");
+        };
+    });
 };
